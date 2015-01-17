77d63d68976ecf95ce03bb05d13d36bb28969311
Alexander Reitzel <funtimecoding@gmail.com>
2015-01-17 21:16:47 +0100

    added bin prefix to package.json attribute

--- init-project.sh
+++ init-project.sh
@@ -26,7 +26,7 @@ sed -i "" -e "s/em/${INITIALS}/g" package.json web/index.html src/ConsoleMain.js
 sed -i "" -e "s/ExampleModule/${CAMEL}/g" package.json web/index.html src/ConsoleMain.js src/WebMain.js src/ExampleModule.js spec/ExampleModuleSpec.js
 sed -i "" -e "s/example-project/${DASH}/g" package.json web/index.html
 sed -i "" -e "s/\"example-script\"/\"${DASH}\"/g" package.json
-sed -i "" -e "s/bin\/example-script/${INITIALS}/g" package.json
+sed -i "" -e "s/bin\/example-script/bin\/${INITIALS}/g" package.json
 
 git mv "src/ExampleModule.js" "src/${CAMEL}.js"
 git mv "spec/ExampleModuleSpec.js" "spec/${CAMEL}Spec.js"
